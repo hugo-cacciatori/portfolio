dbcc2ae818164108d7dd50fbd77e798d405cda60
Hugo <hugo.cacciatori.pro@gmail.com>
2024-06-30 08:33:01 +0200

    added contact

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"bb10c5466638e963479ba5e64e601e42d1a434
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2899382106"
+    serviceWorkerVersion: "3767152988"
   }
 });
